68090b876d0e3e2d720ccc5cd551bdeb03358397
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2019-11-14 21:23:23 -0300

    Code created to plot the accuracies by the subjects.

--- models/fbcsp/evaluation.py
+++ models/fbcsp/evaluation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def print_accuracies(accuracies, subjects):
@@ -12,3 +13,23 @@ def print_accuracies(accuracies, subjects):
 def print_mean_accuracies(accuracies):
     for k in accuracies.keys():
         print("k %s: %s" % (k, np.mean(accuracies[k])))
+
+
+def plot_accuracies_by_subjects(subjects, accuracies):
+    x = np.arange(len(subjects))
+    bar_width = .35
+
+    fig, ax = plt.subplots()
+    for (i, classifier) in enumerate(accuracies.keys()):
+        position = x - bar_width / 2 if i % 2 == 0 else x + bar_width / 2
+        ax.bar(position, accuracies[classifier], bar_width, label=classifier)
+
+    ax.set_ylabel("Accuracy")
+    ax.set_title("Accuracies by subjects")
+    ax.set_xticks(x)
+    ax.set_xticklabels(subjects)
+    ax.legend()
+
+    fig.tight_layout()
+
+    plt.show()
