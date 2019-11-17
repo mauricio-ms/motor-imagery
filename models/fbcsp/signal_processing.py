4475b161b24f4db8a010a25c47a99e505ad1d66b
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2019-11-17 19:47:05 -0300

    Parameters adjusted, comments removed.

--- models/fbcsp/signal_processing.py
+++ models/fbcsp/signal_processing.py
@@ -2,14 +2,13 @@ import numpy as np
 from scipy import signal
 
 FS = 250
-NYQUIST = 0.5 * FS
+NYQUIST = .5 * FS
 
-LOW_FREQS = range(4, 33, 4)
-QUANTITY_BANDS = LOW_FREQS[-1]//LOW_FREQS[0]-1
+LOW_FREQS = range(4, 41, 4)
+QUANTITY_BANDS = len(LOW_FREQS)-1
 
 
 def filter_in_all_frequency_bands(trial):
-    # n_channels = trial.shape[1]
     filtered_signals = np.zeros((QUANTITY_BANDS, *trial.shape))
     for n_low_freq in range(0, len(LOW_FREQS)):
         low_freq = LOW_FREQS[n_low_freq]
@@ -22,8 +21,6 @@ def filter_in_all_frequency_bands(trial):
         b, a = signal.cheby2(5, 0.5, [low_freq, high_freq], btype="bandpass", fs=250)
 
         filtered_signals[n_low_freq, :, :] = signal.filtfilt(b, a, trial, axis=0)
-        # for n_channel in range(n_channels):
-        #     filtered_signals[n_low_freq, :, n_channel] = signal.filtfilt(b, a, trial[:, n_channel])
 
     return filtered_signals
 
@@ -43,5 +40,5 @@ def apply_bandpass_filter(eeg):
 
 
 def bandpass_filter(eeg_signal):
-    b, a = signal.cheby2(9, 50, [4, 32], btype="bandpass", fs=250)
+    b, a = signal.butter(4, [8, 30], btype="bandpass", fs=250)
     return signal.filtfilt(b, a, eeg_signal, axis=1)
